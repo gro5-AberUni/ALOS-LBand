1f8a5c4cd5ccec6919d2d2907e2020696a317612
gro5-AberUni <74013177+gro5-AberUni@users.noreply.github.com>
2024-07-15 16:08:30 +0100

    Update applyXGBoostClassificationImgs_Bins.py
    Removed line which copies output to data, it already does that

--- Scripts/Classification/applyXGBoostClassificationImgs_Bins.py
+++ Scripts/Classification/applyXGBoostClassificationImgs_Bins.py
@@ -785,7 +785,6 @@ if __name__ == "__main__":
         shutil.copy(classifiedImageSieveSlopeM,outputDir)
         os.chdir(cwd)
         shutil.rmtree(workspace)
-        shutil.move(outputDir, '/data/')
     except:
         print('There Has Been An Error')
         os.chdir(cwd)
