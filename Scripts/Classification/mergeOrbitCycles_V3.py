b53b8a978156315e0ba013326d60cf73449aea22
gro5-AberUni <74013177+gro5-AberUni@users.noreply.github.com>
2025-03-05 09:00:18 +0000

    Update mergeOrbitCycles_V3.py
    Initial update to allow for Odd/Even RSP images to be formed per orbit cycle, as well as adding pyramids and stats to the output images

--- Scripts/Classification/mergeOrbitCycles_V3.py
+++ Scripts/Classification/mergeOrbitCycles_V3.py
@@ -25,7 +25,7 @@ for index, row in datesPD.iterrows():
     elif orbitCycle < 100:
         orbitCycle = '0{0}'.format(orbitCycle)
     print(orbitCycle)
-    listOrbitRowsClassDirs = glob.glob('/data/ALOS-Output*C{0}'.format(orbitCycle))
+    listOrbitRowsClassDirs = glob.glob('/data/ALOS-Output*C{0}*'.format(orbitCycle))
     listMergeFiles = []
     for dir in listOrbitRowsClassDirs:
         os.chdir(dir)
@@ -49,4 +49,84 @@ for index, row in datesPD.iterrows():
         clr_lut[7] = '#FFFFFF'
 
         rsgislib.imageutils.define_colour_table(classFile, clr_lut)
+        rsgislib.imageutils.pop_thmt_img_stats(classFile,add_clr_tab=False)
+
+    #### Get Even Orbit Paths ####
+
+    listRSP = []
+    for fn in listMergeFiles:
+        rsp = fn.split('/')[-1].split('_')[2]
+        listRSP.append(rsp)
+    uniqueRSP = np.unique(listRSP)
+
+    listEven = []
+    listOdd = []
+    for rsp in listRSP:
+        rspInt = int(rsp[3:])
+        if rspInt % 2 == 0:
+            listEven.append(rsp)
+        else:
+            listOdd.append(rsp)
+    print(listEven)
+    print(listOdd)
+
+    #### Gather all Even Files ####
+
+    listEvenImg = []
+    for evRSP in listEven:
+        listOrbitRowsClassDirs = glob.glob('/data/ALOS-Output*C{0}_{1}'.format(orbitCycle,evRSP))
+        for dir in listOrbitRowsClassDirs:
+            os.chdir(dir)
+            try:
+                listEvenImg.append(os.path.abspath(glob.glob('*Classified*SLopeM.tif')[0]))
+            except:
+                print('No Classified Image Found')
+            os.chdir(cwd)
+        classFile = '/data/Classified_Output_Orbit-Cycle_{0}-Dated-{1}_{2}_Even-RSP.tif'.format(orbitCycle,row['Start'].replace('/','-'),row['End'].replace('/','-'))
+    if len(listOrbitRowsClassDirs)!=0:
+        rsgislib.imageutils.create_img_mosaic(listEvenImg, classFile, 0, 0, 1,1, 'GTIFF', 1)
+
+        clr_lut = dict()
+        clr_lut[0] = '#000000'
+        clr_lut[1] = '#6CABDD'
+        clr_lut[2] = '#000080'
+        clr_lut[4] = '#004225'
+        clr_lut[5] = '#a7a7a7'
+        clr_lut[6] = '#d21255'
+        clr_lut[7] = '#FFFFFF'
+
+        rsgislib.imageutils.define_colour_table(classFile, clr_lut)
+        rsgislib.imageutils.pop_thmt_img_stats(classFile,add_clr_tab=False)
+                                                
+
+
+    #### Gather all Odd Files ####
+
+    listOddImg = []
+    for oddRSP in listOdd:
+        listOrbitRowsClassDirs = glob.glob('/data/ALOS-Output*C{0}_{1}'.format(orbitCycle,oddRSP))
+        for dir in listOrbitRowsClassDirs:
+            os.chdir(dir)
+            try:
+                listEvenImg.append(os.path.abspath(glob.glob('*Classified*SLopeM.tif')[0]))
+            except:
+                print('No Classified Image Found')
+            os.chdir(cwd)
+        classFile = '/data/Classified_Output_Orbit-Cycle_{0}-Dated-{1}_{2}_Odd-RSP.tif'.format(orbitCycle,row['Start'].replace('/','-'),row['End'].replace('/','-'))
+    if len(listOrbitRowsClassDirs)!=0:
+        rsgislib.imageutils.create_img_mosaic(listEvenImg, classFile, 0, 0, 1,1, 'GTIFF', 1)
+
+        clr_lut = dict()
+        clr_lut[0] = '#000000'
+        clr_lut[1] = '#6CABDD'
+        clr_lut[2] = '#000080'
+        clr_lut[4] = '#004225'
+        clr_lut[5] = '#a7a7a7'
+        clr_lut[6] = '#d21255'
+        clr_lut[7] = '#FFFFFF'
+
+        rsgislib.imageutils.define_colour_table(classFile, clr_lut)
+        rsgislib.imageutils.pop_thmt_img_stats(classFile,add_clr_tab=False)
+        
+        
 
